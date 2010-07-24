d284ab95d94bf0a3c4b7a67a4091ae1d01d65d56
RobertStewart <robert@wombatnation.com>
2010-07-24 10:28:54 +0800

    Updated both appenders to log to error handler if insert fails, instead of throwing an exception

--- src/main/java/com/google/code/log4mongo/MongoDbAppender.java
+++ src/main/java/com/google/code/log4mongo/MongoDbAppender.java
@@ -33,6 +33,7 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
+import com.mongodb.MongoException;
 
 
 /**
@@ -177,7 +178,12 @@ public class MongoDbAppender
         
         if (bson != null)
         {
-            collection.insert(bson);
+            try {
+                getCollection().insert(bson);
+            } catch (MongoException e) {
+                errorHandler.error("Failed to insert document to MongoDB", e,
+                               ErrorCode.WRITE_FAILURE);
+            }
         }
     }
     
